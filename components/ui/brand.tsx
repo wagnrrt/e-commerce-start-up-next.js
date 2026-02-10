4e1f90626f53d9f33263a3e03f1dbb6a3bbd0570
wagnrrt <wagnrb0@gmail.com>
2026-02-09 22:26:03 -0300

    style: add header layout

---
+++ components/ui/brand.tsx
@@ -0,0 +1,4 @@
+const Brand = (props) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path fill="#000" d="M17 9A5 5 0 0 0 7 9v6a5 5 0 0 0 10 0v-4h-2v4a2 2 0 1 1-4 0V9a2 2 0 1 1 4 0v1h2z" /></svg>
+);
+export default Brand;
